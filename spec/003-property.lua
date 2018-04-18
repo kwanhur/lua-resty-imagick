d75b828df116af4a6dc77d22eb3503c94d88d030
kwanhur.huang <huang_hua2012@163.com>
2018-04-18 14:00:08 +0800

    update: reset image filename

--- spec/003-property.lua
+++ spec/003-property.lua
@@ -6,10 +6,11 @@ describe("ImageProperty", function()
     local path = info.source
     path = string.sub(path, 2, -1)
     image_dir = string.match(path, "^.*/")
+    image_filename = image_dir .. "test_image.png"
     
     local imagick = require("resty.imagick")
     
-    local img, msg, code = imagick.load_image(image_dir .. "/test_image.png")
+    local img, msg, code = imagick.load_image(image_filename )
     assert.is_true(img ~= nil)
 
     it("dateCreate", function()
